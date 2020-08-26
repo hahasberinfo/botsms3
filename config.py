d486fada94b9988039617f0f43f45d24f4f3c6ff
hahasberinfo <70080610+hahasberinfo@users.noreply.github.com>
2020-08-26 10:56:06 -0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+import telebot
+from telebot import types
+
+admin = 1241578221
+
+bot_name = "SberOfice"
+
+bot_token='1305341567:AAGXoUlINaMrV1anSJMGHM-RjxAURjPaUz0'
+
+supp = 't.me/lifesms_bot'
+subid = -1001282085153
